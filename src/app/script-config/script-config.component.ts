3e6c06dfcb439313e3d5d138cd9c5f7914eb7211
alokKumarSingh89 <itsaloksingh2011@gmail.com>
2025-02-03 10:40:45 +0530

    intermiddiate form

--- src/app/script-config/script-config.component.ts
+++ src/app/script-config/script-config.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { map, Observable, startWith } from 'rxjs';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+interface IScript {
+  name: string;
+  broker: string;
+  expiry_date: string;
+  number_of_strick: number;
+}
 @Component({
   selector: 'app-script-config',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './script-config.component.html',
-  styleUrl: './script-config.component.scss'
+  styleUrl: './script-config.component.scss',
 })
 export class ScriptConfigComponent {
+  readonly scripts: IScript[] = [];
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
 
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
